19753bc97e5dcf00a0a44212961c1237caa7a6f7
saintfeng <fengshen186@gmail.com>
2014-01-11 22:06:52 +0800

    printOutDegrees_uniform
    printOutDegrees_uniform

--- morgen/scripts/env.sh
+++ morgen/scripts/env.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-
-
-export TINY_GRAPH="/home/chengyichao/food/tiny"
-export FLA_GRAPH="/home/chengyichao/food/FLA-road"
-export MESH_GRAPH="/home/chengyichao/food/mesh.6pt"
-export KKT_GRAPH="/home/chengyichao/food/kkt_power"
-export COPAPER_GRAPH="/home/chengyichao/food/coPapers"
-export AUDI_GRAPH="/home/chengyichao/food/audikw1"
-export RMAT1_GRAPH="/home/chengyichao/food/rmat.5Mv.60Me"
-export RMAT2_GRAPH="/home/chengyichao/food/rmat.2Mv.100Me"
-export AMAZON_GRAPH="/home/chengyichao/food/Amazon"
-export THERMAL_GRAPH="/home/chengyichao/food/thermal2"
-export LIVE_GRAPH="/home/chengyichao/food/LiveJournal1"
-export ECO_GRAPH="/home/chengyichao/food/ecology1"
-export RANDOM1_GRAPH="/home/chengyichao/food/random.2Mv.128Me"
-export RANDOM2_GRAPH="/home/chengyichao/food/random.2Mv.100Me"
-
-
-
+export TINY_GRAPH="/home/scc11/lifeng/food/tiny"
+export FLA_GRAPH="/home/scc11/lifeng/food/FLA-road"
+export MESH_GRAPH="/home/scc11/lifeng/food/mesh.6pt"
+export KKT_GRAPH="/home/scc11/lifeng/food/kkt_power"
+export COPAPER_GRAPH="/home/scc11/lifeng/food/coPapers"
+export AUDI_GRAPH="/home/scc11/lifeng/food/audikw1"
+export RMAT1_GRAPH="/home/scc11/lifeng/food/rmat.5Mv.60Me"
+export RMAT2_GRAPH="/home/scc11/lifeng/food/rmat.2Mv.100Me"
+export AMAZON_GRAPH="/home/scc11/lifeng/food/Amazon"
+export THERMAL_GRAPH="/home/scc11/lifeng/food/thermal2"
+export LIVE_GRAPH="/home/scc11/lifeng/food/LiveJournal1"
+export ECO_GRAPH="/home/scc11/lifeng/food/ecology1"
+export RANDOM1_GRAPH="/home/scc11/lifeng/food/random.2Mv.128Me"
+export RANDOM2_GRAPH="/home/scc11/lifeng/food/random.2Mv.100Me"
